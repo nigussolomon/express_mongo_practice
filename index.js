ff3f99c5efad15a172dbba5ed70684804b373a07
nigussolomon <nigussolomon444@gmail.com>
2023-01-06 00:57:23 +0300

    clean up

--- index.js
+++ index.js
@@ -24,46 +24,45 @@ const stds = mongoose.model(
   "Students"
 );
 
-stds.find({}, function (err, std) {
-  if (err) {
-    console.log(handleError(err));
-  } else {
-    console.log(std);
-  }
-});
-
 app.get("/students", (req, res) => {
-	const first_name = req.query.fname
-  stds.findOne({first_name: first_name}, function (err, std) {
+  const first_name = req.query.fname;
+  stds.findOne({ first_name: first_name }, function (err, std) {
     if (err) {
-      res.send({ 'Success': false,'error': handleError(err)});
+      res.send({ Success: false, error: handleError(err) });
     } else {
-      res.send({'Success': true,'data': std});
+      res.send({ Success: true, data: std });
     }
   });
 });
 
 app.post("/students/new", (req, res) => {
-	const first_name = req.body.fname
-	const last_name = req.body.lname
-	const section = req.body.sec
-  stds.create({first_name: first_name, last_name: last_name, section: section}, function (err, std) {
-    if (err) {
-      res.send({ 'Success': false,'error': handleError(err)});
-    } else {
-      res.send({'Success': true,'data': std});
+  const first_name = req.body.fname;
+  const last_name = req.body.lname;
+  const section = req.body.sec;
+  stds.create(
+    { first_name: first_name, last_name: last_name, section: section },
+    function (err, std) {
+      if (err) {
+        res.send({ Success: false, error: handleError(err) });
+      } else {
+        res.send({ Success: true, data: std });
+      }
     }
-  });
+  );
 });
 
 app.put("/students", (req, res) => {
-	const first_name = req.query.fname
-	const last_name = req.body.lname
-  stds.update({first_name: first_name}, {$set: {last_name: last_name}}, function (err, std) {
-    if (err) {
-      res.send({ 'Success': false,'error': handleError(err)});
-    } else {
-      res.send({'Success': true,'msg': std});
+  const first_name = req.query.fname;
+  const last_name = req.body.lname;
+  stds.update(
+    { first_name: first_name },
+    { $set: { last_name: last_name } },
+    function (err, std) {
+      if (err) {
+        res.send({ Success: false, error: handleError(err) });
+      } else {
+        res.send({ Success: true, msg: std });
+      }
     }
-  });
+  );
 });
