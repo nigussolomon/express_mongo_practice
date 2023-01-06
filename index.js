ff85b52265772bfecc7c3ae1d2c581b2fc8914b4
nigussolomon <nigussolomon444@gmail.com>
2023-01-06 12:11:59 +0300

    added removed configs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 require('./config')(mongoose)
 require('./controllers/students')(app, mongoose)
 
