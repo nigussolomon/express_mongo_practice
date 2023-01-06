3e719e3cbf32d383a2b040dfd2846025f3ee3e0a
nigussolomon <nigussolomon444@gmail.com>
2023-01-06 12:40:28 +0300

    removed .env file and added config file

--- config.js
+++ config.js
@@ -1,10 +1,14 @@
-require('dotenv').config()
-module.exports = function (mongoose) {
-  mongoose.set("strictQuery", true);
-  const conn = mongoose
-    .connect(process.env.HOST, {
-      dbName: process.env.DB_NAME,
-    })
-    .then(() => console.log("Succesfully Connected to MongoDB"))
-    .catch((error) => console.log(error));
-};
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+    path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
+});
+
+module.exports = {
+    ENV_TYPE : process.env.NODE_ENV || 'development',
+    HOST : process.env.HOST || 'localhost',
+    PORT : process.env.PORT || 3030,
+    DB_HOST: process.env.DB_HOST || 'mongodb://localhost:27017',
+    DB_NAME: process.env.DB_NAME || 'SampleDB'
+}
