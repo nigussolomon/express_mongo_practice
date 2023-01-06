07cad251c683fd6fcfbb36463020bc7cfe9badd4
nigussolomon <nigussolomon444@gmail.com>
2023-01-06 12:15:19 +0300

    imported courses routes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 require('./config')(mongoose)
 require('./controllers/students')(app, mongoose)
+require('./controllers/courses')(app, mongoose)
 
 
 
