b2d528de32d0f540de2f7b62fec8022a42272d9b
nigussolomon <nigussolomon444@gmail.com>
2023-01-07 20:29:49 +0300

    updated config file for node update

--- config.js
+++ config.js
@@ -9,6 +9,6 @@ module.exports = {
     ENV_TYPE : process.env.NODE_ENV || 'development',
     HOST : process.env.HOST || 'localhost',
     PORT : process.env.PORT || 3030,
-    DB_HOST: process.env.DB_HOST || 'mongodb://localhost:27017',
+    DB_HOST: process.env.DB_HOST || 'mongodb://127.0.0.1:27017',
     DB_NAME: process.env.DB_NAME || 'SampleDB'
 }
